4b39f5a13b26a27553c0d041a16fc6d6dab2a5c5
jm01146 <59844100+jm01146@users.noreply.github.com>
2024-02-24 22:06:04 -0500

    Update comms_arduino.py
    Making it to an understandable package

--- comms_arduino.py
+++ comms_arduino.py
@@ -1,28 +1,34 @@
 import serial.tools.list_ports
 
-ports = serial.tools.list_ports.comports()
-serialInst = serial.Serial()
+def list_ports():
+    ports = serial.tools.list_ports.comports()
+    ports_list = [str(one_port) for one_port in ports]
+    return ports_list
 
-portsList = []
+def select_port():
+    printPorts = print(list_ports())
+    ports_list = list_ports()
+    val = input('Select Port: COM')
+    for port in ports_list:
+        if port.startswith('COM' + str(val)):
+            return 'COM' + str(val)
+    raise ValueError("Port not found")
 
-for onePort in ports:
-    portsList.append(str(onePort))
-    print(str(onePort))
+def open_serial(port):
+    serial_inst = serial.Serial()
+    serial_inst.baudrate = 9600
+    serial_inst.port = port
+    serial_inst.open()
+    return serial_inst
 
-val = input('Select Port: COM')
+def send_command(serial_inst):
+    while True:
+        command = input('Arduino Command: (ON/OFF); ')
+        serial_inst.write(command.encode('utf-8'))
+        if command == 'exit':
+            break
 
-for x in range(0, len(portsList)):
-    if portsList[x].startswith('COM' + str(val)):
-        portVar = 'COM' + str(val)
-        print(portVar)
-
-serialInst.baudrate = 9600
-serialInst.port = portVar
-serialInst.open()
-
-while True:
-    command = input('Arduino Command: (ON/OFF); ')
-    serialInst.write(command.encode('utf-8'))
-
-    if command == 'exit':
-        exit()
+# Example to run the entire code
+#     port = select_port()
+#     serial_connection = open_serial(port)
+#     send_command(serial_connection)
