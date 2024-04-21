754a509da90ab7cb0d1f96b63f0457277356ba2c
John Marcial <59844100+jm01146@users.noreply.github.com>
2024-04-21 17:34:20 -0400

    Update CommsArduino.py

--- CommsArduino.py
+++ CommsArduino.py
@@ -3,20 +3,21 @@ import serial.tools.list_ports
 import re
 
 
+# Creating the Ports class for the GUI to use
 class Ports(serial.Serial):
     def __init__(self):
         super().__init__()
-        # Taking serialInst adn scanningPorts as Objects that have the properties of Serial Functions
+        # Taking serialInst and scanningPorts as Objects that have the properties of Serial Functions
         self.serialInst = serial.Serial()
         self.scanningPorts = serial.tools.list_ports.comports()
 
-        # Setting the values that will be gathered from the GUI
+        # Setting the initial values that will be gathered from the GUI
         self.serialInst.baudrate = 9600
         self.portName = None
         self.portList = None
         self.com = None
 
-    # Making a function that Prints all the Ports
+    # Making a function that prints all the ports
     def list_port(self):
         ports_listed = []
         for ports in self.scanningPorts:
@@ -24,26 +25,26 @@ class Ports(serial.Serial):
         self.portList = ports_listed[:]
         return self.portList
 
-    # Way for the GUI software to select the baudrate
+    # Way for the GUI to select the baudrate
     def baudrate_selection(self, gui_baudrate):
         self.serialInst.baudrate = gui_baudrate
 
-    # Way for the GUI software to select the COM Device
+    # Way for the GUI to select the COM channel
     def comm_selection(self, gui_com):
         self.com = gui_com
         match = re.search(r'COM(\d+)', self.com)
         if match:
             self.com = str(match.group())
 
-    # Way for the GUI software to connect to COM Device
+    # Way for the GUI to connect to device
     def connect(self):
         self.serialInst.port = self.com
         self.serialInst.open()
 
-    # Way for the GUI software to send to COM Device
+    # Way for the GUI to send commands to device
     def send(self, command):
         self.serialInst.write(command.encode('utf-8'))
 
-    # Way for the GUI software to disconnect from COM device
+    # Way for the GUI to disconnect from the device
     def disconnect(self):
         self.serialInst.close()
